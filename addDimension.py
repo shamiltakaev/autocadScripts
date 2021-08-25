3a25ff210503e50caa01ceb84b96c8211106122d
Shamil Takaev <Shamilchechya@gmail.com>
2021-08-25 18:24:53 +0300

    Edit dimensions

--- addDimension.py
+++ addDimension.py
@@ -5,13 +5,20 @@ from pyautocad.types import APoint
 
 app = Autocad()
 
-lays = list(app.get_selection("Select Object"))
-
-# f = lays[0]
-
+lays = list(app.get_selection("Select Object"))[0]
+# print(lays.InsertionPoint)
 
 for i in range(len(lays) - 1):
-    p1 = APoint(lays[i].Center)
-    p2 = APoint(lays[i+1].Center)
+    
+    #region Pts
+    p1 = APoint(lays[i].InsertionPoint)
+    p2 = APoint(lays[i+1].InsertionPoint)
+    #endregion
 
+    #region GeometryFigure
+    # p1 = APoint(lays[i].Center)
+    # p2 = APoint(lays[i + 1].Center)
+    #endregion
+
+    # app.model.DimLineInside(p1, p2, p1)
     app.model.AddDimAligned(p1, p2, p1)
